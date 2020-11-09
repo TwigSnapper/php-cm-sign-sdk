47325e86cebebe379fc36bd155570e484f6f7e68
Chris Smits <c.smits@jool.nl>
2020-11-09 13:57:10 +0100

    Added PHPDoc to CmAuthTokenInterface

--- src/CmAuthTokenInterface.php
+++ src/CmAuthTokenInterface.php
@@ -2,7 +2,17 @@
 
 namespace CmSignSdk;
 
+/**
+ * Interface CmAuthTokenInterface
+ * @package CmSignSdk
+ */
 interface CmAuthTokenInterface
 {
+    /**
+     * @param string $keyId
+     * @param string $secret
+     * @param int $expiresAfter
+     * @return string
+     */
     public function createToken(string $keyId, string $secret, int $expiresAfter = 60): string;
 }
