c4ba5ae525a4c2c0091c9a4778fa9dcd31c9d9f1
Chris Smits <c.smits@jool.nl>
2020-11-09 13:57:52 +0100

    Implemented addOwners, addFiles and addInvitees dossier builder methods

--- src/DossierBuilder.php
+++ src/DossierBuilder.php
@@ -3,6 +3,9 @@
 namespace CmSignSdk;
 
 use CmSignSdk\Entity\Dossier;
+use CmSignSdk\Entity\File;
+use CmSignSdk\Entity\Invitee;
+use CmSignSdk\Entity\Owner;
 
 /**
  * Class DossierBuilder
@@ -46,6 +49,48 @@ class DossierBuilder implements DossierBuilderInterface
         if ($seconds < 60) $seconds = 60;
         if ($seconds > 7776000) $seconds = 7776000;
         $this->dossier->setExpiresIn($seconds);
+        return $this;
+    }
+
+    /**
+     * @param Owner[] $owners
+     * @return mixed
+     */
+    public function addOwners(array $owners): DossierBuilder
+    {
+        $this->dossier->setOwners(array_merge(
+            $this->dossier->getOwners(),
+            $owners
+        ));
+
+        return $this;
+    }
+
+    /**
+     * @param File[] $files
+     * @return DossierBuilder
+     */
+    public function addFiles(array $files): DossierBuilder
+    {
+        $this->dossier->setFiles(array_merge(
+            $this->dossier->getFiles(),
+            $files
+        ));
+
+        return $this;
+    }
+
+    /**
+     * @param Invitee[] $invitees
+     * @return DossierBuilder
+     */
+    public function addInvitees(array $invitees): DossierBuilder
+    {
+        $this->dossier->setInvitees(array_merge(
+            $this->dossier->getInvitees(),
+            $invitees
+        ));
+
         return $this;
     }
 }
--- src/DossierBuilderInterface.php
+++ src/DossierBuilderInterface.php
@@ -2,8 +2,43 @@
 
 namespace CmSignSdk;
 
+use CmSignSdk\Entity\File;
+use CmSignSdk\Entity\Invitee;
+use CmSignSdk\Entity\Owner;
+
+/**
+ * Interface DossierBuilderInterface
+ * @package CmSignSdk
+ */
 interface DossierBuilderInterface
 {
+    /**
+     * @param string $locale
+     * @return DossierBuilder
+     */
     public function setLocale(string $locale): DossierBuilder;
+
+    /**
+     * @param int $seconds
+     * @return DossierBuilder
+     */
     public function expiresIn(int $seconds): DossierBuilder;
+
+    /**
+     * @param Owner[] $owners
+     * @return mixed
+     */
+    public function addOwners(array $owners): DossierBuilder;
+
+    /**
+     * @param File[] $files
+     * @return DossierBuilder
+     */
+    public function addFiles(array $files): DossierBuilder;
+
+    /**
+     * @param Invitee[] $invitees
+     * @return DossierBuilder
+     */
+    public function addInvitees(array $invitees): DossierBuilder;
 }
